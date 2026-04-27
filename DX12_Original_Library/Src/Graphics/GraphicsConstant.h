32d9c4ff45e75d0e467fd8a41ecd4bedf987762e
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-04-27 13:55:46 +0900

    ヘッダーの完成

---
+++ DX12_Original_Library/Src/Graphics/GraphicsConstant.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <windows.h>
+
+// DX12の初期化や描画に関する定数を作成する
+constexpr UINT FRAME_BUFFER_COUNT{ 2 };
--- DX12_Original_Library/Src/Graphics/GraphicsDevice.h
+++ DX12_Original_Library/Src/Graphics/GraphicsDevice.h
@@ -3,6 +3,7 @@
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <wrl/client.h>
+#include "GraphicsConstant.h"
 using Microsoft::WRL::ComPtr;
 
 #pragma comment(lib, "d3d12.lib")
@@ -14,6 +15,9 @@ class GraphicsDevice
 public:
 	// シングルトン化
 	static GraphicsDevice& Instance();
+
+	// デフォルトデストラクタ
+	~GraphicsDevice() = default;
 
 	// 初期化処理(ウィンドウハンドルと画面横サイズ、縦サイズ)
 	void Initialize(HWND _hwnd, int _width, int _height);
@@ -29,4 +33,31 @@ public:
 	ID3D12Device* GetDevice() const;
 	// コマンドリストのGetter
 	ID3D12GraphicsCommandList* GetCommandList() const;
+
+private:
+	GraphicsDevice() = default; // 内部でのみのインスタンス
+
+	// コピーの禁止
+	GraphicsDevice(const GraphicsDevice&) = delete;
+	GraphicsDevice& operator=(const GraphicsDevice&) = delete;
+
+private:
+	// GPUとの接続
+	ComPtr<IDXGIFactory6> factory; // GPU列挙用
+	ComPtr<ID3D12Device> device; // GPU通信の中心
+
+	// 命令送信
+	ComPtr<ID3D12CommandQueue> cmdQueue; // 命令の投入先
+	ComPtr<ID3D12CommandAllocator> cmdAllocators[FRAME_BUFFER_COUNT]; // ダブルバッファ用のアロケーター
+	ComPtr<ID3D12GraphicsCommandList> cmdList; // 命令記録
+	ComPtr<ID3D12Fence> fence; // CPUとGPUの同期
+	UINT64 fenceValues[FRAME_BUFFER_COUNT]; // 各フレームの同期値
+
+	// 画面表示
+	ComPtr<IDXGISwapChain4> swapChain; // バッファ交換
+	ComPtr<ID3D12Resource> backBuffers[FRAME_BUFFER_COUNT]; // ダブルバッファ
+	ComPtr<ID3D12DescriptorHeap> rtvHeap; // RTV用のヒープ
+	UINT rtvDescriptorSize; // RTV一つのサイズ
+	UINT currentFrameIndex; // 今どちらのバッファか
+
 };
