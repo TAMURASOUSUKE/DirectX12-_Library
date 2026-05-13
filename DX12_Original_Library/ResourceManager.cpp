eb1a95ac22476f1be1be3e4f5d49fe2a2d34e79c
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-05-13 16:03:28 +0900

    頂点バッファのリソースオブジェクト生成

---
+++ DX12_Original_Library/ResourceManager.cpp
@@ -0,0 +1,33 @@
+#include "ResourceManager.h"
+
+void ResourceManager::Initialize(ID3D12Device* _device)
+{
+	if (_device != nullptr)
+	{
+		device = _device;
+	}
+}
+
+GPUBuffer ResourceManager::CreateVertexBuffer(const void* _data, UINT _dataSize, UINT _strideSize)
+{
+	D3D12_HEAP_PROPERTIES heapProperties{}; // 頂点ヒープの設定
+	heapProperties.Type = D3D12_HEAP_TYPE_UPLOAD; // アップロードヒープに設定
+	heapProperties.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN; // ページング
+
+	D3D12_RESOURCE_DESC resDesc{}; // リソース設定構造体
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER; // バッファとして使う
+	resDesc.Width = _dataSize; // 頂点バッファのサイズ
+	resDesc.Height = 1; // バッファは1D
+	resDesc.DepthOrArraySize = 1; // 配列ではない
+	resDesc.MipLevels = 1; // ミップマップなし
+	resDesc.Format = DXGI_FORMAT_UNKNOWN; // バッファはフォーマットなし
+	resDesc.SampleDesc = { 1, 0 }; // MSAAなし
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR; // メモリが最初から最後まで連続していることを示す
+
+	GPUBuffer buffer{};
+	HRESULT result{}; // 結果が成功しているかどうか調べる溜めの変数
+	// UploadHeap上にバッファリソースを作成する
+	result = device->CreateCommittedResource(&heapProperties, D3D12_HEAP_FLAG_NONE, &resDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&buffer.resource));
+	if (FAILED(result)) return; // 失敗していたら終了
+	
+}
