ef63e4bfc2f4faad16638ede0b34b827e1ecf1b0
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-05-31 18:56:04 +0900

    キーの名前空間と.hの作成

--- DX12_Original_Library/Src/Input/keyboardInput.h
+++ DX12_Original_Library/Src/Input/keyboardInput.h
@@ -1,7 +1,16 @@
 #pragma once
+#include "InputName.h"
 
 // keyboardの入力を簡易化するクラス
 class KeyboardInput
 {
+public:
+	void Update(); // 入力更新
 
+	bool IsPressed(BYTE _key); // 押している間
+	bool IsPushed(BYTE _key); // 押した瞬間
+	bool IsReleased(BYTE _key); // 話した瞬間
+private:
+	BYTE currentKeys[256]{};
+	BYTE prevKeys[256]{};
 };
