c3e0c7991bdec46287619495b40a23d6dc94c68e
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-06-03 09:58:24 +0900

    デバッグ

--- DX12_Original_Library/GraphicsTest/GraphicsTest.cpp
+++ DX12_Original_Library/GraphicsTest/GraphicsTest.cpp
@@ -42,6 +42,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	TextureData player{ Gfx::LoadTexture("Res/player.png") }; // Playerのハンドル取得
 
 	Vector2 playerPos{ 100.0f, 100.0f };
+	float ang{ 0.0f }; // 角度加算用のテスト
 
 	while (Gfx::ProcessMessage())
 	{
@@ -57,14 +58,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 		playerPos += dir * 8.0f;
 
+		ang += 0.01f;
+		ang = Math::NormalizeAngle(ang); // 角度の正規化
+
 		Gfx::ClearScreen(); // 画面クリア(黒)
 
 		// スプライトバッチテスト
 		Gfx::DrawSprite(background, Vector2{ 0.0f, 0.0f }, Vector2{ 1280.0f, 720.0f });
 		Gfx::DrawSprite(player, playerPos, Vector2{ 128.0f, 128.0f });
 		Gfx::DrawSprite(player, Vector2{playerPos.x, playerPos.y + 100.0f}, Vector2{128.0f, 128.0f}, 30.0f * Math::DEG_TO_RAD);
-		Gfx::DrawSprite(player, Vector2{playerPos.x, playerPos.y + 200.0f}, Vector2{128.0f, 128.0f});
-		Gfx::DrawSprite(player, Vector2{playerPos.x, playerPos.y + 300.0f}, Vector2{128.0f, 128.0f});
+		Gfx::DrawSprite(player, Vector2{playerPos.x, playerPos.y + 200.0f}, Vector2{128.0f, 128.0f}, 80.0f * Math::DEG_TO_RAD);
+		Gfx::DrawSprite(player, Vector2{playerPos.x, playerPos.y + 300.0f}, Vector2{128.0f, 128.0f}, ang);
 		Gfx::DrawSprite(enemy, Vector2{ 800.0f, 400.0f }, Vector2{ 128.0f, 128.0f });
 
 		TSLib::EndFrame(); // フレーム終了処理
