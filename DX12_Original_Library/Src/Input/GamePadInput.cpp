d47db50968e47503c8d43db63cc5d4c3f3a1d30b
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-07-02 15:19:56 +0900

    パッドの入力受付完成

---
+++ DX12_Original_Library/Src/Input/GamePadInput.cpp
@@ -0,0 +1,35 @@
+﻿#include <windows.h>
+#include "GamePadInput.h"
+
+void GamePadInput::Update()
+{
+	prevPad = currentPad; // 状態保存
+	XINPUT_STATE state{};
+	DWORD result{ XInputGetState(0, &state) };
+	if (ERROR_SUCCESS == result)
+	{
+		currentPad = state.Gamepad;
+	}
+	else
+	{
+		currentPad = {}; // 入力は残さない
+	}
+}
+
+// 押されている間(1フレーム目からみる)
+bool GamePadInput::IsPress(int _key)
+{
+	return (currentPad.wButtons & _key);
+}
+
+// 押した瞬間
+bool GamePadInput::IsPushed(int _key)
+{
+	return (currentPad.wButtons & _key) && !(prevPad.wButtons & _key);
+}
+
+// 離した瞬間
+bool GamePadInput::IsReleased(int _key)
+{
+	return !(currentPad.wButtons & _key) && (prevPad.wButtons & _key);
+}
---
+++ DX12_Original_Library/Src/Input/GamePadInput.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <XInput.h>
+#include "InputBase.h"
+#pragma comment(lib, "Xinput.lib")
+
+// コントローラーからの入力を受け取る(現在はXboxのみで今後Switch,PSへ拡張)
+class GamePadInput : public InputBase
+{
+public:
+	void Update() override; // 入力更新
+
+	bool IsPress(int _key) override; // 押している間
+	bool IsPushed(int _key) override; // 押した瞬間
+	bool IsReleased(int _key) override; // 離した瞬間
+
+private:
+	XINPUT_GAMEPAD currentPad{}; // 現在の入力
+	XINPUT_GAMEPAD prevPad{}; // 1フレーム前の入力
+};
