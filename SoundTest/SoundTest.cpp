be5df62568351455860d12b84d0e1772273e8021
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-07-09 23:33:24 +0900

    サウンドロードプロジェクト

---
+++ SoundTest/SoundTest.cpp
@@ -0,0 +1,24 @@
+#include "../DX12_Original_Library/Src/Facade/TSLib.h"
+
+// 音のテスト等を行う
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	// 初期化 失敗したら-1を返す
+	if (!TSLib::Initialize(L"SoundTest", 1280, 720)) return -1;
+
+	DescriptorHandle
+
+	while (Gfx::ProcessMessage())
+	{
+		TSLib::BeginFrame(); // フレーム開始処理
+
+
+
+		Gfx::ClearScreen(); // 画面クリア(黒)
+
+
+		TSLib::EndFrame(); // フレーム最後の処理
+	}
+
+	TSLib::Finish(); // 終了処理
+}
