c86ccc74b145cc8c87c24c2f0c946a49153fdfeb
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-07-23 10:16:25 +0900

    vcxprojに残っていたGSの履歴削除とGfxInternalのInitializeに残っていた未使用HRESULTの削除

--- DX12_Original_Library/Src/Facade/Gfx.cpp
+++ DX12_Original_Library/Src/Facade/Gfx.cpp
@@ -380,8 +380,6 @@ namespace {
 // 初期化処理(これを呼ぶだけで初期化処理が済むようにする)
 bool GfxInternal::Initialize(const wchar_t* _title, int _width, int _height)
 {
-	HRESULT result{};
-
 	screenWidth = _width;
 	screenHeight = _height;
 	// 前回の初期化状態を引き継がない
--- DX12_Original_Library/Src/Facade/Gfx.h
+++ DX12_Original_Library/Src/Facade/Gfx.h
@@ -1,5 +1,4 @@
 ﻿#pragma once
-#include <cstddef>
 #include <memory>
 #include <type_traits>
 #include "../Core/Handle/TexHandle.h"
