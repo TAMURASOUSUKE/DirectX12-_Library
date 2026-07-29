959cc9c6d65d4bb450f0e2c4294060f2b445d353
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-07-29 22:15:21 +0900

    SDK側のデバッグ機能の有効化

--- DX12_Original_Library/TSGameLibSDK/include/TSGameLib/Facade/System.h
+++ DX12_Original_Library/TSGameLibSDK/include/TSGameLib/Facade/System.h
@@ -4,15 +4,18 @@
 // OSやアプリケーション全体に関する機能を提供する
 namespace System
 {
+	enum class WindowMode
+	{
+		Windowed, // Windowモード
+		BorderlessFullscreen // ボーダーレスフルスクリーン
+	};
+
 	// タイトルバーを除いた、実際に描画できる領域を取得する
 	Vector2Int GetClientSize();
-
 	// ウィンドウタイトルを変更する
 	void SetWindowTitle(const wchar_t* _title);
-
 	// ウィンドウが現在操作対象になっているか
 	bool IsWindowFocused();
-
 	// ゲームの終了を要求する
 	void RequestQuit();
 }
