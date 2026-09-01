4b89f96ebaf5d99e599e6ec38e11e621cc494fe8
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-09-01 13:53:27 +0900

    静的モデルとスキニングモデルの構造体の作成

--- DX12_Original_Library/Src/Graphics/ModelRenderQueue.h
+++ DX12_Original_Library/Src/Graphics/ModelRenderQueue.h
@@ -1,1 +1,35 @@
 #pragma once
+#include <vector>
+#include <span>
+#include <variant>
+#include "GraphicsConstant.h"
+#include "../Core/Handle/ModelHandle.h"
+#include "../Component/Transform.h"
+#include "../Core/Handle/AnimInstanceHandle.h"
+
+// 静的モデル一つ分の描画コマンド
+struct StaticModelRenderCommand
+{
+	ModelHandle modelHandle{};
+	Transform transform{};
+};
+
+// スキニングモデル一つ分の描画コマンド
+struct SkinningModelRenderCommand
+{
+	AnimInstanceHandle animInstanceHandle{};
+	Transform transform{};
+};
+
+// 静的モデルまたはスキニングモデルのどちらか一方を保持する
+using ModelRenderCommand = std::variant<StaticModelRenderCommand, SkinningModelRenderCommand>;
+
+// 1フレームのモデル描画依頼を保存
+class ModelRenderQueue
+{
+public:
+
+
+private:
+
+};
